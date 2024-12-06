b475bde1e21b53207e33dad1f915b084f81bec57
awps <asurdu@awesomemotive.com>
2024-12-06 12:46:26 +0200

    Unnecessary `echo`

--- views/listings.php
+++ views/listings.php
@@ -51,7 +51,7 @@ $addons     = $api->get_addons();
 
 			<?php if ( TemplateTags\current_user_can_purchase_addons() && ! empty( $addons['addons'] ) ) : ?>
 				<div class="wp101-addon-notice">
-					<h2><?php echo esc_html_e( 'More from WP101', 'wp101' ); ?></h2>
+					<h2><?php esc_html_e( 'More from WP101', 'wp101' ); ?></h2>
 					<p><?php esc_html_e( 'Get the most out of WP101 with even more content!', 'wp101' ); ?></p>
 					<p><a href="<?php echo esc_url( menu_page_url( 'wp101-addons', false ) ); ?>" class="button button-secondary"><?php esc_html_e( 'Get more videos from WP101', 'wp101' ); ?></a></p>
 				</div>
